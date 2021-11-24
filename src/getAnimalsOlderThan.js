8916e6fdec72af0beff2affd0ff7e6c2bf9419d7
Joao <joaovictorcgjvcg@gmail.com>
2021-11-24 14:44:50 -0400

    Entrega requisito 1 e 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,7 +2,7 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) =>
-  species.find((species) => species.name === animal)
-  .residents.every((residents) => residents.age >= age);
+  species.find((specie) => specie.name === animal)
+  .residents.every((resident) => resident.age >= age);
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,7 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-
-}
+const getSpeciesByIds = (...ids) => 
+  species.filter((specie) => ids.includes(specie.id))
 
 module.exports = getSpeciesByIds;

1bd6119da7568af5ea25521417fd9d3c1bdf029a
Joao <joaovictorcgjvcg@gmail.com>
2021-11-24 18:12:36 -0400

    Entrega requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,16 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function isManager(id) {
-  // seu código aqui
-}
+const isManager = (id) =>
+  employees.some((employee) => employee.managers.includes(id));
 
-function getRelatedEmployees(managerId) {
-  // seu código aqui
-}
+const getRelatedEmployees = (managerId) => {
+  if (isManager(managerId)) {
+    return employees
+      .filter((manager) => manager.managers.includes(managerId))
+      .map((manager) => `${manager.firstName} ${manager.lastName}`);
+  }
+  throw new Error("O id inserido não é de uma pessoa colaboradora gerente!");
+};
 
 module.exports = { isManager, getRelatedEmployees };

9dd38177de2b8601c237300ab98b664a6a7f429d
Joao <joaovictorcgjvcg@gmail.com>
2021-11-24 15:16:28 -0400

    Entrega requisito 1 e 2 com correcao do lint

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -3,6 +3,6 @@ const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) =>
   species.find((specie) => specie.name === animal)
-  .residents.every((resident) => resident.age >= age);
+    .residents.every((resident) => resident.age >= age);
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,7 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (...ids) => 
-  species.filter((specie) => ids.includes(specie.id))
+const getSpeciesByIds = (...ids) =>
+  species.filter((specie) => ids.includes(specie.id));
 
 module.exports = getSpeciesByIds;

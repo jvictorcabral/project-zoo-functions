f95e7e37f6a5ecb11199b7484d35c04bbd95db3c
Joao <joaovictorcgjvcg@gmail.com>
2021-12-31 19:04:04 -0400

    Entrega do requisito 9 com correcao do lint

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -2,7 +2,8 @@ const { species, employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 const getOldestFromFirstSpecies = (id) => {
-  const firstAnimal = employees.find((employe) => employe.id === id).responsibleFor[0];
+  const firstAnimal = employees.find((employe) => employe.id === id)
+    .responsibleFor[0];
   const animals = species.find((specie) => specie.id === firstAnimal).residents;
   const oldestAnimal = animals.reduce((acc, animal) => {
     if (acc.age > animal.age) return acc;
@@ -11,6 +12,6 @@ const getOldestFromFirstSpecies = (id) => {
   const array = [];
   array.push(oldestAnimal.name, oldestAnimal.sex, oldestAnimal.age);
   return array;
-}
+};
 
 module.exports = getOldestFromFirstSpecies;

46812861da0da503477f9f780d62e75ad56465d3
Joao <joaovictorcgjvcg@gmail.com>
2021-12-31 16:53:19 -0400

    Entrega do requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,24 @@
 const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {
-  // seu código aqui
-}
+const countEntrants = (entrants) => {
+  const child = entrants.filter((entrant) => entrant.age < 18).length;
+  const adult = entrants.filter(
+    (entrant) => entrant.age >= 18 && entrant.age < 50
+  ).length;
+  const senior = entrants.filter((entrant) => entrant.age >= 50).length;
+
+  return { child, adult, senior };
+};
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || entrants.length === undefined) {
+    return 0;
+  }
+  return (
+    countEntrants(entrants).child * 20.99 +
+    countEntrants(entrants).adult * 49.99 +
+    countEntrants(entrants).senior * 24.99
+  );
 }
 
 module.exports = { calculateEntry, countEntrants };

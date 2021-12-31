f84c889efda274411108223ed62def1e3f6f5706
Joao <joaovictorcgjvcg@gmail.com>
2021-12-31 19:00:31 -0400

    Entrega do requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,16 @@
+const { species, employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+const getOldestFromFirstSpecies = (id) => {
+  const firstAnimal = employees.find((employe) => employe.id === id).responsibleFor[0];
+  const animals = species.find((specie) => specie.id === firstAnimal).residents;
+  const oldestAnimal = animals.reduce((acc, animal) => {
+    if (acc.age > animal.age) return acc;
+    return animal;
+  });
+  const array = [];
+  array.push(oldestAnimal.name, oldestAnimal.sex, oldestAnimal.age);
+  return array;
 }
 
 module.exports = getOldestFromFirstSpecies;

c37e5d4f84fffb539dd9d1b2c324ee7a67d04042
Joao <joaovictorcgjvcg@gmail.com>
2022-01-08 14:01:21 -0400

    Entrgando o requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,25 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
-}
+const countAnimals = (animal) => {
+  if (animal === undefined) {
+    return species.reduce((acc, specie) => {
+      acc[specie.name] = specie.residents.length;
+      return acc;
+    }, {});
+  }
+
+  const findAnimals = species.find(
+    (specie) => specie.name === animal.specie
+  ).residents;
+  if (animal.sex === undefined) {
+    return findAnimals.length;
+  }
+  return findAnimals.reduce((acc, specie) => {
+    return specie.sex === animal.sex ? acc + 1 : acc;
+  }, 0);
+};
+
+console.log(countAnimals);
 
 module.exports = countAnimals;

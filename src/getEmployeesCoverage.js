208688a4eb0b25773dc147b7024bddc446ac92d3
Joao <joaovictorcgjvcg@gmail.com>
2022-01-13 11:35:16 -0400

    Entrega do requisito 10 finalmente, Deus tarda mas nao falha

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,36 @@
+const { species, employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const infos = (info) => ({
+  id: info.id,
+  fullName: `${info.firstName} ${info.lastName}`,
+  species: species
+    .filter((specie) => info.responsibleFor.includes(specie.id))
+    .map((specie) => specie.name),
+  locations: species
+    .filter((specie) => info.responsibleFor.includes(specie.id))
+    .map((specie) => specie.location),
+});
+
+const byId = (id) => {
+  if (employees.some((employee) => employee.id === id)) {
+    const info = employees.find((employee) => employee.id === id);
+    return infos(info);
+  }
+  throw new Error('Informações inválidas');
+};
+
+function getEmployeesCoverage(param) {
+  if (!param) {
+    return employees.map((info) => infos(info));
+  }
+  const { name, id } = param;
+  if (name) {
+    const info = employees
+      .find((employee) => employee.firstName === name || employee.lastName === name);
+    return infos(info);
+  }
+  return byId(id);
 }
 
 module.exports = getEmployeesCoverage;
